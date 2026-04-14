47d83a4450e90777cf1f054790aac90bfc0b75f5
Obi Obetta <obettastephnie@gmail.com>
2026-04-14 11:15:19 -0500

    tests skeleton

---
+++ tests/test_gap_detector.py
@@ -0,0 +1,22 @@
+import pytest
+from src.monitor import GapDetector
+
+class TestGapDetector:
+
+  def test_no_gap_on_first_packet(self):
+    pass
+
+  def test_no_gap_on_sequential(self):
+    pass
+
+  def test_detects_single_gap(self):
+    pass
+
+  def test_detects_multi_gap(self):
+    pass
+
+  def test_out_of_order_ignored(self):
+    pass
+
+  def test_cumulative_gap_tracking(self):
+    pass
