27e446fbde86e7545a785af7d9a4e592c8726aa7
Obi Obetta <obettastephnie@gmail.com>
2026-04-16 00:34:43 -0500

    full initial implementation of monitor.py

--- src/monitor.py
+++ src/monitor.py
@@ -3,7 +3,7 @@ import struct
 import json
 import argparse
 
-from prometheus_client import start_wsgi_server
+from prometheus_client import start_http_server
 from src.exporter import packets_received, gaps_detected, last_sequence_id
 
 MCAST_GRP = "239.255.0.1"
@@ -28,11 +28,39 @@ class GapDetector:
     return gap
     
 
-def join_multicast():
-  pass
+def join_multicast() -> socket.socket:
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.bind(("", MCAST_PORT))
+  mreq = struct.pack("4sL", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+  s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+  return s
 
-def run():
-  pass
+def run(metrics_port: int = 8000):
+  start_http_server(metrics_port)
+  print(f"Metrics can be found at http://localhost:{metrics_port}/metrics")
+
+  s: socket.socket = join_multicast()
+  detector = GapDetector()
+
+  print(f"Monitor joined {MCAST_GRP}:{MCAST_PORT}")
+  while True:
+    data, _ = s.recvfrom(1024)
+    message = json.loads(data.decode())
+    sequence = message["sequence_id"]
+
+    gap = detector.process(sequence)
+
+    packets_received.inc()
+    last_sequence_id.set(sequence)
+
+    if gap > 0:
+      gaps_detected.inc(gap)
+      print(f"[GAP DETECTED] received {sequence} (missing {gap})")
 
 if __name__ == "__main__":
-  run()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--port", type=int, default=8000)
+  args = parser.parse_args()
+  run(args.port)
+  
